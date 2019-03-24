e16860efdc5f84341f3fd278d089e1394b19782b
Jakub Kowalik <kowalik.kub@gmail.com>
2019-03-24 14:28:56 +0100

    Added Dockerfile with working container (#2)
    * Testing deployment
    
    * Added working Dockerfile

--- src/application.py
+++ src/application.py
@@ -4,6 +4,7 @@ import tornado.web
 from request_handlers.api import Index
 from request_handlers.errors import NotFoundErrorHandler
 
+
 class Application(object):
     def __init__(self, config):
         self.config = config
@@ -15,18 +16,19 @@ class Application(object):
         pass
 
 
-class TornadoApplication(Application):
+class SPPApi(Application):
     def __init__(self,
                  config,
                  route_builder):
         super().__init__(config)
         self.route_builder = route_builder
-        self.app = tornado.web.Application([
+        self.app = tornado.web.Application(handlers=[
             (self.route_builder.create('index'), Index)
         ],
-            debug=self.config['debug'],
+            default_host=self.config.host,
+            debug=self.config.debug,
             default_handler_class=NotFoundErrorHandler)
-        self.app.listen(config['networking']['port'])
+        self.app.listen(config.port)
 
     def run(self):
         tornado.ioloop.IOLoop.current().start()
--- src/config.py
+++ src/config.py
@@ -1,21 +1,44 @@
 import json
+import re
 import logging as log
 
 class ConfigLoader(object):
     def load(self):
         pass
 
+    @staticmethod
+    def create(file_path):
+        if '.json' in file_path:
+            return JsonConfigLoader(file_path)
+
+
+
+class Config(object):
+    def __init__(self):
+        self.debug = None
+        self.routes = None
+        self.port = None
+    
+    def load(self, file_path):
+        config = ConfigLoader.create(file_path).load()
+
+        self.debug = config['debug']
+        self.routes = config['networking']['routes']
+        self.port = config['networking']['port']
+        self.host = config['networking']['host']
+        return self
+
 
 class JsonConfigLoader(ConfigLoader):
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, file_path):
+        self.file_path = file_path
         self.config = None
 
     def load(self):
         try:
-            with open(self.filename) as fs:
+            with open(self.file_path) as fs:
                 self.config = json.load(fs)
                 return self.config
         except IOError as e:
             raise IOError("""Couldn't load config file.
-                            Error message {e}""")
+                            Error message {0}""".format(e))
