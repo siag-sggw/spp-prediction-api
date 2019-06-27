113b69df3c7c79c7fc7ff6d2719e4e4462dbb6b1
Jakub Kowalik <kowalik.kub@gmail.com>
2019-06-27 18:27:08 +0200

    Changed image upload destination (#12)

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 DOCKER_REPOSITORY='kowalikjakub/spp_api'
-docker tag spp_api $DOCKER_REPOSITORY
+
 
 echo "Deploying branch ${TRAVIS_BRANCH}"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 if [ $TRAVIS_BRANCH == 'master' ]
 then
+    docker tag spp_api $DOCKER_REPOSITORY
     docker push $DOCKER_REPOSITORY
 elif [ $TRAVIS_BRANCH == 'develop' ]
 then
+
+    docker tag spp_api:dev $DOCKER_REPOSITORY
     docker push "$DOCKER_REPOSITORY:dev"
 else
     echo "No matching configuration for ${TRAVIS_BRANCH}, skipping deploy stage"
